670d7ffd00aa20afa3af8bda28b3e00ed5ebe3bb
Elijah Wood <ej.thewoodies@gmail.com>
2024-10-05 00:26:06 +0100

    updated

--- cmd/Project/main.go
+++ cmd/Project/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+
+	"github.com/ItsElijahWood/Goproject/internal/routes"
 )
 
 func main() {
     log.Println("Listening on :8080...")
-    err := routes.startServer()
+    err := routes.StartServer()
     if err != nil {
         log.Fatal(err)
     }
