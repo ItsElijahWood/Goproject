af87f79e135982397e1b6931f3f1597b12bfb044
Elijah Wood <ej.thewoodies@gmail.com>
2024-10-05 01:02:24 +0100

    updated into 2 files easy to read

--- cmd/Project/main.go
+++ cmd/Project/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/ItsElijahWood/Goproject/internal/routes"
 )
 
 func main() {
+    routes.SetupRoutes()
+
     log.Println("Listening on :8080...")
-    err := routes.StartServer()
+    err := http.ListenAndServe(":80", nil)
     if err != nil {
         log.Fatal(err)
     }
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -4,8 +4,7 @@ import (
 	"net/http"
 )
 
-func startServer() error {
+func SetupRoutes() {
     fs := http.FileServer(http.Dir("../../cmd/Project/static"))
     http.Handle("/", fs)
-    return http.ListenAndServe(":80", nil)
 }
